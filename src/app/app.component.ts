936fb9204ad7fe645e249457df0d37a8591191d8
Aymen Mehri <aymen.mehri@soat.fr>
2017-03-30 15:32:23 +0200

    Add counter with Rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import { HomeService } from './app.service';
 import { Component, OnInit } from '@angular/core';
 import * as _ from 'lodash';
 
 import { Store } from '@ngrx/store';
 import { INCREMENT, DECREMENT, RESET } from './../store/counter';
+
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/scan';
 
 interface AppState {
   counter: number;
@@ -21,8 +28,25 @@ export class AppComponent implements OnInit {
 
   counter: Observable<number>;
 
+  click$ = new Subject();
+  clock;
+
   constructor(private store: Store<AppState>) {
     this.counter = store.select('counter');
+
+    this.clock = Observable
+      .merge(this.click$.mapTo('hour'), Observable.interval(1000).mapTo('second'))
+      .startWith(new Date().toString())
+      .scan((acc, value, index) => {
+        const date = new Date(acc);
+        if (value === 'second') {
+          date.setSeconds(date.getSeconds() + 1);
+        }
+        if (value === 'hour') {
+          date.setHours(date.getHours() + 1);
+        }
+        return date;
+      });
   }
 
   increment() {
