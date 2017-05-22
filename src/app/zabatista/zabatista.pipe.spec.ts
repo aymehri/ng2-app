18170a4dcf208a0345242be4cb9d81f7371b00ab
Aymen Mehri <aymen.mehri@soat.fr>
2017-05-22 11:33:45 +0200

    test pipe

--- src/app/zabatista/zabatista.pipe.spec.ts
+++ src/app/zabatista/zabatista.pipe.spec.ts
@@ -1,8 +1,17 @@
 import { ZabatistaPipe } from './zabatista.pipe';
 
 describe('ZabatistaPipe', () => {
+
   it('create an instance', () => {
     const pipe = new ZabatistaPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('test convert seconds to Date', () => {
+    const pipe = new ZabatistaPipe();
+    const time = 30000;
+    const formatDate = pipe.transform(time);
+    expect(formatDate).toBe('08:20:00');
+  });
+
 });
